497acabe202ada953f4074b2268356690f32ee0d
zaqueuRod <146400058+zaqueuRod@users.noreply.github.com>
2026-09-04 19:13:44 -0300

    corrigir conexão com firebase

--- src/Paginas/Cadastro.jsx
+++ src/Paginas/Cadastro.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { db } from './firebase'
+import { db } from "../firebase.js";
 import { ref, push, set, query, orderByChild, equalTo, get } from 'firebase/database'
 
 export default function Cadastro(props) {
